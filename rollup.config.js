49658c7acdfe2172ebf37f6bfc6008027a7343ec
Filidor Wiese <fili@fili.nl>
2018-05-23 14:26:48 +0200

    Changes module output format to UMD

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: 'src/spriteling.js',
   output: {
     file: 'dist/spriteling.min.js',
-    format: 'cjs'
+    name: 'SpriteLing',
+    format: 'umd'
   },
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    commonjs(),
   ]
-};
+}
