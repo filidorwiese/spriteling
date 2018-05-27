cc16784a684ea385e912772ab3eaf3853c2cdb30
Filidor Wiese <fili@fili.nl>
2018-05-27 12:49:25 +0200

    Outputs minified and non-minified version

---
+++ examples/spriteling.js
@@ -0,0 +1,1 @@
+../dist/spriteling.js
--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,17 +1,19 @@
 import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default {
   input: 'src/spriteling.ts',
   output: {
-    file: 'dist/spriteling.js',
+    file: 'dist/spriteling.min.js',
     name: 'Spriteling',
     format: 'umd'
   },
   plugins: [
     resolve(),
     typescript(),
-    commonjs()
+    commonjs(),
+    uglify()
   ]
 }
