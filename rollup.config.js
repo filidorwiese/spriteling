72428236025b82c8e0faf40d7ff06290b0395c69
Filidor Wiese <fili@fili.nl>
2018-07-01 13:58:24 +0200

    Adds ESM/CJS outputs to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,34 @@
 import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
 
-export default {
-  input: 'src/spriteling.ts',
-  output: {
-    file: 'dist/spriteling.js',
-    name: 'Spriteling',
-    format: 'umd'
+export default [
+  {
+    input: 'src/spriteling.ts',
+    output: {
+      file: 'dist/spriteling.js',
+      name: 'Spriteling',
+      format: 'esm'
+    },
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs()
+    ]
   },
-  plugins: [
-    resolve(),
-    typescript(),
-    commonjs()
-  ]
-}
+  {
+    input: 'src/spriteling.ts',
+    output: {
+      file: 'dist/spriteling.min.js',
+      name: 'Spriteling',
+      format: 'umd'
+    },
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs(),
+      uglify()
+    ]
+  }
+]
